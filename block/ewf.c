3d665778cc0209912144fd5882cc569b82f4b1e6
Thomas Liebetraut <thomas@tommie-lie.de>
2015-11-24 17:31:18 +0100

    ewf: Add bfio io "class" to access Qemu's block driver. This is later used as io backend for libewf and will allow EWF access through the Qemu block driver, e.g. over an NBD or HTTP link or EWF inside QCOW2.

--- block/ewf.c
+++ block/ewf.c
@@ -28,6 +28,7 @@
 #include "qapi/qmp/qstring.h"
 
 #include <libewf.h>
+#include <libbfio.h>
 
 //#define DEBUG_EWF
 
@@ -37,16 +38,281 @@
 #define DPRINTF(fmt, ...) do { } while (0)
 #endif
 
+typedef struct qemu_libbfio_io_handle {
+    BlockDriverState *file;
+    off64_t offset;
+    bool opened;
+} qemu_libbfio_io_handle;
 
 typedef struct BDRVEwfState {
 } BDRVEwfState;
 
+
+int qemu_libbfio_io_handle_initialize(qemu_libbfio_io_handle **io_handle,
+                                      libbfio_error_t **errp);
+int qemu_libbfio_io_handle_clone(qemu_libbfio_io_handle **destination_io_handle,
+                                 qemu_libbfio_io_handle *source_io_handle,
+                                 libbfio_error_t **error);
+int qemu_libbfio_io_handle_free(qemu_libbfio_io_handle **io_handle,
+                                libbfio_error_t **error);
+int qemu_libbfio_io_handle_set_file(qemu_libbfio_io_handle *io_handle,
+                                    BlockDriverState *file,
+                                    libbfio_error_t **error);
+int qemu_libbfio_open(qemu_libbfio_io_handle *io_handle, int access_flags,
+                      libbfio_error_t **error);
+int qemu_libbfio_close(qemu_libbfio_io_handle *io_handle, libbfio_error_t **error);
+ssize_t qemu_libbfio_read(qemu_libbfio_io_handle *io_handle, uint8_t *buffer, size_t size,
+                          libbfio_error_t **error);
+ssize_t qemu_libbfio_write(qemu_libbfio_io_handle *io_handle, const uint8_t *buffer,
+                           size_t size, libbfio_error_t **error);
+off64_t qemu_libbfio_seek_offset(qemu_libbfio_io_handle *io_handle, off64_t offset,
+                                 int whence, libbfio_error_t **error);
+int qemu_libbfio_exists(qemu_libbfio_io_handle *io_handle, libbfio_error_t **error);
+int qemu_libbfio_is_open(qemu_libbfio_io_handle *io_handle, libbfio_error_t **error);
+int qemu_libbfio_get_size(qemu_libbfio_io_handle *io_handle, size64_t *size,
+                          libbfio_error_t **error);
 
 static int ewf_probe(const uint8_t *buf, int buf_size, const char *filename);
 static int ewf_open(BlockDriverState *bs, QDict *options, int flags,
                     Error **errp);
 static void ewf_close(BlockDriverState *bs);
 static void bdrv_ewf_init(void);
+
+
+int qemu_libbfio_io_handle_initialize(qemu_libbfio_io_handle **io_handle,
+                                      libbfio_error_t **errp) {
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+
+    if (*io_handle) {
+        DPRINTF("invalid qemu IO handle value already set.\n");
+        return -1;
+    }
+
+    *io_handle = g_malloc0(sizeof(qemu_libbfio_io_handle));
+    return 1;
+}
+
+int qemu_libbfio_io_handle_clone(qemu_libbfio_io_handle **destination_io_handle,
+                                 qemu_libbfio_io_handle *source_io_handle,
+                                 libbfio_error_t **error) {
+    if (!destination_io_handle) {
+        DPRINTF("invalid destination qemu IO handle.\n");
+        return -1;
+    }
+    if (!*destination_io_handle) {
+        DPRINTF("destination qemu IO handle already set.");
+        return -1;
+    }
+    if (source_io_handle == 0) {
+        *destination_io_handle = 0;
+        return -1;
+    }
+
+    if (qemu_libbfio_io_handle_initialize(destination_io_handle, error) != 1) {
+        DPRINTF("unable to create qemu IO handle.");
+        return -1;
+    }
+
+    memmove(*destination_io_handle, source_io_handle,
+            sizeof(qemu_libbfio_io_handle));
+
+    return 1;
+}
+
+int qemu_libbfio_io_handle_free(qemu_libbfio_io_handle **io_handle,
+                                libbfio_error_t **error) {
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+    g_free(*io_handle);
+    *io_handle = 0;
+
+    return 1;
+}
+
+int qemu_libbfio_io_handle_set_file(qemu_libbfio_io_handle *io_handle,
+                                    BlockDriverState *file,
+                                    libbfio_error_t **error) {
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+
+    io_handle->file = file;
+
+    return 1;
+}
+
+int qemu_libbfio_open(qemu_libbfio_io_handle *io_handle, int access_flags,
+                      libbfio_error_t **error) {
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+    if (!io_handle->file) {
+        DPRINTF("invalid qemu bdrv state.\n");
+        return -1;
+    }
+
+    // nothing else to do here, the bds is not managed by us
+    io_handle->opened = 1;
+    return 1;
+}
+
+int qemu_libbfio_close(qemu_libbfio_io_handle *io_handle, libbfio_error_t **error) {
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+    if (!io_handle->file) {
+        DPRINTF("invalid qemu bdrv state.\n");
+        return -1;
+    }
+
+    // nothing else to do here, the bds is not managed by us
+    io_handle->opened = 0;
+    return 1;
+}
+
+ssize_t qemu_libbfio_read(qemu_libbfio_io_handle *io_handle, uint8_t *buffer, size_t size,
+                          libbfio_error_t **error) {
+    int ret;
+
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+    if (!io_handle->file) {
+        DPRINTF("invalid qemu bdrv state.\n");
+        return -1;
+    }
+
+    ret = bdrv_pread(io_handle->file, io_handle->offset, buffer, size);
+
+    if (ret < 0) {
+        DPRINTF("could not read from qemu bds: %d\n", ret);
+        return -1;
+    }
+    io_handle->offset += ret;
+    return ret;
+}
+
+ssize_t qemu_libbfio_write(qemu_libbfio_io_handle *io_handle, const uint8_t *buffer,
+                           size_t size, libbfio_error_t **error) {
+    int ret;
+
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+    if (!io_handle->file) {
+        DPRINTF("invalid qemu bdrv state.\n");
+        return -1;
+    }
+
+    ret = bdrv_pwrite(io_handle->file, io_handle->offset, buffer, size);
+
+    if (ret < 0) {
+        DPRINTF("could not write to qemu bds: %d\n", ret);
+        return -1;
+    }
+    io_handle->offset += ret;
+    return ret;
+}
+
+off64_t qemu_libbfio_seek_offset(qemu_libbfio_io_handle *io_handle, off64_t offset,
+                                 int whence, libbfio_error_t **error) {
+    int64_t filesize = bdrv_getlength(io_handle->file);
+    off64_t new_offset = 0;
+
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+    if (!io_handle->file) {
+        DPRINTF("invalid qemu bdrv state.\n");
+        return -1;
+    }
+
+    if (filesize < 0) {
+        DPRINTF("could not determine qemu bds size: %"PRId64".\n", filesize);
+        return -1;
+    }
+
+    switch (whence) {
+        case SEEK_SET:
+            new_offset = offset;
+            break;
+        case SEEK_CUR:
+            new_offset = io_handle->offset + offset;
+            break;
+        case SEEK_END:
+            new_offset = filesize - offset;
+            break;
+        default:
+            break;
+    }
+
+    if (new_offset < 0 || new_offset > filesize) {
+        DPRINTF("invalid seek offset %"PRId64".\n", new_offset);
+        return -1;
+    }
+    io_handle->offset = new_offset;
+    return io_handle->offset;
+}
+
+int qemu_libbfio_exists(qemu_libbfio_io_handle *io_handle, libbfio_error_t **error) {
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+
+    // we handle a non-existing bdrv as ENOENT
+    if (!io_handle->file) {
+        return 0;
+    }
+    return 1;
+}
+
+int qemu_libbfio_is_open(qemu_libbfio_io_handle *io_handle, libbfio_error_t **error) {
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+    if (!io_handle->file) {
+        return 0;
+    }
+
+    return io_handle->opened;
+}
+
+int qemu_libbfio_get_size(qemu_libbfio_io_handle *io_handle, size64_t *size,
+                          libbfio_error_t **error) {
+    int64_t ret;
+
+    if (!io_handle) {
+        DPRINTF("invalid qemu IO handle.\n");
+        return -1;
+    }
+    if (!io_handle->file) {
+        DPRINTF("invalid qemu bdrv state.\n");
+        return -1;
+    }
+
+    ret = bdrv_getlength(io_handle->file);
+    if (ret < 0) {
+        DPRINTF("could not determine bdrv length.\n");
+        *size = 0;
+        return -1;
+    }
+
+    *size = ret;
+    return 1;
+}
 
 
 static int ewf_probe(const uint8_t *buf, int buf_size, const char *filename)
